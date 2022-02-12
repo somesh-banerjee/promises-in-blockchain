309e733d957f9fc40f7bbc74b106f37b45b37d62
SOMESH BANERJEE <54861461+somesh-banerjee@users.noreply.github.com>
2022-02-12 22:01:33 +0530

    Organized layout

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
-import React from 'react'
+import React from 'react';
+import Layout from './components/Layout';
 
 const App = () => {
   return (
     <div>
-      
+      <Layout>
+        
+      </Layout>
     </div>
   )
 }
---
+++ src/components/Header.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Header = () =>{
+	return (
+		<div style={{font:'small-caps bold 48px Georgia, serif'}}>
+          <p style={{color:'black',marginTop:'20px',marginBottom:'1px'}}>Promise Keeper</p>
+          <hr style={{top:'1px',left:'1px',height:'5px',backgroundColor:'black'}}/>
+        </div>
+	);
+};
+
+export default Header;
