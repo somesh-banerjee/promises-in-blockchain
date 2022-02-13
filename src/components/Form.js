e0b92703f1aa7f8d248427da6056a271d96169ca
SOMESH BANERJEE <54861461+somesh-banerjee@users.noreply.github.com>
2022-02-13 06:10:02 +0530

    Update Form.js
    Changing to class component

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,34 +1,55 @@
-import React from 'react';
-import { Button,Form,Message } from 'semantic-ui-react';
+import React,{ Component } from 'react';
+import { Button,Form,Message,Segment } from 'semantic-ui-react';
 
-const aForm = () => {
-  return (
-    <div>
-      <Form size="big">
-        <h2>Make your Promise</h2>
-        <Form.Group>
-            <Form.Input
-              label='Name'
-              width={4}
-              placeholder='Name'
-            //value={this.state.name}
-            //onChange={event => this.setState({ name: event.target.value })}
-            />
-            <Form.Input
-              label='The Promise'
-              width={11}
-              placeholder='The Promise'
-            //value={this.state.promisest}
-            //onChange={event => this.setState({ promisest: event.target.value })}
-            />
-        </Form.Group>
-            <Message error header="Oops!" />
-            <Button primary>
+class aForm extends Component {
+  state = {
+		name: '',
+		promiseStmnt: '',
+		errorMessage: '',
+		loading: false
+	};
+
+  onSubmit = async (event) => {
+    event.preventDefault();
+
+    this.setState({ loading: true })
+
+    
+
+    this.setState({ loading: false })
+  }
+
+  render(){
+    return (
+      <div>
+        <Segment inverted>
+        <Form size="big" inverted onSubmit={this.onSubmit} error={!!this.state.errorMessage}>
+          <h2>Make your Promise</h2>
+          <Form.Group>
+              <Form.Input
+                label='Name'
+                width={4}
+                placeholder='Name'
+                value={this.state.name}
+                onChange={event => this.setState({ name: event.target.value })}
+              />
+              <Form.Input
+                label='The Promise'
+                width={11}
+                placeholder='The Promise'
+                value={this.state.promiseStmnt}
+                onChange={event => this.setState({ promiseStmnt: event.target.value })}
+              />
+          </Form.Group>
+            <Message error header="Oops!" content={this.state.errorMessage}/>
+            <Button primary loading={this.state.loading}>
                 Make Promise
             </Button>
         </Form>
-    </div>
-  )
+         </Segment>
+      </div>
+    )
+  }
 }
 
  export default aForm;
