efdb6918dcfad2d711819bb7b22264240fc657ee
SOMESH BANERJEE <54861461+somesh-banerjee@users.noreply.github.com>
2022-02-14 16:04:34 +0530

    Update App.js
    Adding support for non metamask user

--- src/App.js
+++ src/App.js
@@ -2,13 +2,31 @@ import React from 'react';
 import Layout from './components/Layout';
 import Form from './components/Form';
 import List from './components/List';
+import { Message } from 'semantic-ui-react';
+
+const checkWeb3 = () => {
+  if(typeof window !== 'undefined' && window.web3 !== undefined){
+    return (
+      <div>
+        <Form />
+        <List />
+      </div>
+    );
+  }else{
+    return (
+      <div>
+        <Message error header="Oops!" content="Looks like Metamask is not installed. Please install it to use the app."/>
+      </div>
+    );
+  }
+}
 
 const App = () => {
+
   return (
     <div>
       <Layout>
-        <Form />
-        <List />
+        {checkWeb3()}
       </Layout>
     </div>
   )
