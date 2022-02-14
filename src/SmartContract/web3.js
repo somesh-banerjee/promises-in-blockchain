b78a941753ad3cf6ba87c28fcec59101b50bea01
SOMESH BANERJEE <54861461+somesh-banerjee@users.noreply.github.com>
2022-02-14 16:03:48 +0530

    Fix

--- src/SmartContract/web3.js
+++ src/SmartContract/web3.js
@@ -1,12 +1,16 @@
 import Web3 from 'web3';
-
+require('dotenv').config()
 let web3;
 
-if(typeof window !== 'undefined' && window.ethereum !== 'undefined'){
-	web3 = new Web3(window.ethereum.currentProvider);
+if(typeof window !== 'undefined' && window.web3 !== undefined){
+	console.log("normal");
+	console.log(window.web3 !== 'undefined');
+	web3 = new Web3(window.web3.currentProvider);
 } else {
+	console.log("not metamask");
 	const provider = new Web3.providers.HttpProvider(
-		'https://rinkeby.infura.io/v3/0087327b8d6e452a890953f7ce0986b4'// + process.env.INFURA_KEY
+		'https://rinkeby.infura.io/v3/' + process.env.INFURA_KEY
+		//'http://127.0.0.1:7545'
 	);
 	web3 = new Web3(provider);
 }
